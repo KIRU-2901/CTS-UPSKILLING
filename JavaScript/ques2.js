689ce25a96abf32fa90bc4f52a5bc89d2a736d20
KIRU-2901 <klutzykiru29@gmail.com>
2026-05-30 20:23:26 +0530

    Update ques2.js

---
+++ JavaScript/ques2.js
@@ -0,0 +1,6 @@
+const eventName = "Music Festival";
+const eventDate = "2026-06-01";
+let seats = 50;
+console.log(`${eventName} on ${eventDate} has ${seats} seats`);
+seats--;
+console.log("Remaining Seats: " + seats);
