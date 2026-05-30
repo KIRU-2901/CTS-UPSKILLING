782ed2f990e2d2ab7ebee2b7240f79982e82ef7a
KIRU-2901 <klutzykiru29@gmail.com>
2026-05-30 20:24:53 +0530

    Update ques7.js

---
+++ JavaScript/ques7.js
@@ -0,0 +1,7 @@
+const container =
+    document.querySelector("#eventContainer");
+const card =
+    document.createElement("div");
+card.textContent =
+    "Music Festival";
+container.appendChild(card);
