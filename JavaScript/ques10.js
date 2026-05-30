2d2e051b8e99fbe4fa8640ba72a7d2a3cbe94d8a
KIRU-2901 <klutzykiru29@gmail.com>
2026-05-30 20:22:08 +0530

    Update ques10.js

---
+++ JavaScript/ques10.js
@@ -0,0 +1,15 @@
+const eventDetails = {
+    name: "Music Festival",
+    date: "2026-06-01"
+};
+const {
+    name,
+    date
+} = eventDetails;
+console.log(name);
+console.log(date);
+const clonedEvents = [
+
+    ...eventList
+];
+console.log(clonedEvents);
